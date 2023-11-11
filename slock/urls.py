7663c149e6e78514a46076ec2a7f5b9ff98d60ea
Wagner P. de Lima <wagnerdelima2@gmail.com>
2023-11-11 14:53:02 -0300

    Add foto na list de empresas

--- slock/urls.py
+++ slock/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from backend.Controller import AutomationController
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('gestao/', include('backend.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
