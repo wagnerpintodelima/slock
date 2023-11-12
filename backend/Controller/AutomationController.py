79d07d98634d1158c02d957d15104b19b21a4a77
Wagner P. de Lima <wagnerdelima2@gmail.com>
2023-11-12 09:17:33 -0300

    Add index de automacao

--- backend/Controller/AutomationController.py
+++ backend/Controller/AutomationController.py
@@ -1,13 +1,32 @@
+import datetime
+import json
+from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib import messages
 
-from backend.models import Automation
+from backend.models import Automation, Company
+
+
+
+def indexView(request):
+    data = Automation.objects.filter(status=True)
+
+    context = {
+        'data': data
+    }
+
+    return render(request, 'Automation/index.html', context)
 
 
 def newView(request):
-    context = {}
+
+    companies = Company.objects.filter(status=True)
+
+    context = {
+        'companies': companies
+    }
     return render(request, 'Automation/new.html', context)
 
 
@@ -15,6 +34,7 @@ def newView(request):
 def SaveAction(request):
 
     if request.method == 'POST':
+        company_id = request.POST.get('company_id', None)
         name = request.POST.get('name', None)
         short_name = request.POST.get('short_name', None)
         ip = request.POST.get('ip', None)
@@ -25,7 +45,14 @@ def SaveAction(request):
         description = request.POST.get('description', None)
         status = request.POST.get('status', 0)
 
+        if not company_id:
+            messages.add_message(request, messages.ERROR, 'Empresa é obrigatório!')
+            return redirect('AutomationNewAction')
+
+        company = Company.objects.get(id=company_id)
+
         item = Automation()
+        item.company = company
         item.name = name
         item.ip = ip
         item.subnet = subnet
@@ -36,8 +63,23 @@ def SaveAction(request):
         item.type_location = type_location
         item.status = status
         item.created_by = request.user.id
+        item.created_at = datetime.datetime.now()
         item.save()
 
         messages.add_message(request, messages.SUCCESS, 'Registro salvo com sucesso!')
 
-    return redirect('AutomationNewAction')
+    return redirect('AutomationIndexView')
+
+
+@login_required
+def deleteAction(request, automation_id):
+
+    item = Automation.objects.get(id=automation_id)
+    item.delete()
+
+    context = {
+        'status': 200,
+        'descricao': 'Excluído com sucesso'
+    }
+
+    return HttpResponse(json.dumps(context, ensure_ascii=False), content_type="application/json")
--- backend/Routes/Automation/urls.py
+++ backend/Routes/Automation/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from backend.Controller import AutomationController as ac
 
 urlpatterns = [
+    path('', ac.indexView, name="AutomationIndexView"),
     path('new', ac.newView, name="AutomationNewAction"),
-    path('save', ac.SaveAction, name="AutomationSaveAction")
+    path('save', ac.SaveAction, name="AutomationSaveAction"),
+    path('delete/<int:automation_id>', ac.deleteAction, name="AutomationDeleteAction"),
 ]
